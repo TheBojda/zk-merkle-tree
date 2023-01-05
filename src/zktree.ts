9008317f4899e878538f2e54ac2883febb56c5e4
Laszlo Fazekas <thebojda@gmail.com>
2023-01-05 04:53:07 +0100

    calculate roots

--- src/zktree.ts
+++ src/zktree.ts
@@ -9,7 +9,7 @@ function calculateHash(mimc, left, right) {
     return BigNumber.from(mimc.F.toString(mimc.multiHash([left, right])))
 }
 
-export function generateZeros(levels: number, mimc: any) {
+export function generateZeros(mimc: any, levels: number) {
     let zeros = []
     zeros[0] = ZERO_VALUE
     for (let i = 1; i <= levels; i++)
@@ -18,11 +18,23 @@ export function generateZeros(levels: number, mimc: any) {
 }
 
 
-export async function calculateMerkleRoot(levels: number, elements: any[]) {
+export function calculateMerkleRoot(mimc: any, levels: number, elements: any[]) {
     const capacity = 2 ** levels
     if (elements.length > capacity) throw new Error('Tree is full')
 
-    const mimc = await buildMimcSponge();
+    const zeros = generateZeros(mimc, levels);
+    let layers = []
+    layers[0] = elements.slice()
+    for (let level = 1; level <= levels; level++) {
+        layers[level] = []
+        for (let i = 0; i < Math.ceil(layers[level - 1].length / 2); i++) {
+            layers[level][i] = calculateHash(
+                mimc,
+                layers[level - 1][i * 2],
+                i * 2 + 1 < layers[level - 1].length ? layers[level - 1][i * 2 + 1] : zeros[level - 1],
+            )
+        }
+    }
 
-
+    return layers[levels].length > 0 ? layers[levels][0] : zeros[levels - 1]
 }
--- test/zktree_test.ts
+++ test/zktree_test.ts
@@ -3,7 +3,7 @@ import { ethers } from "hardhat";
 import { buildMimcSponge, mimcSpongecontract, buildPedersenHash } from 'circomlibjs'
 import { bigInt } from 'snarkjs'
 import { ZKTreeTest } from "../typechain-types";
-import { generateZeros } from '../src/zktree'
+import { generateZeros, calculateMerkleRoot } from '../src/zktree'
 
 const SEED = "mimcsponge";
 
@@ -27,12 +27,24 @@ describe("ZKTree Smart contract test", () => {
         mimc = await buildMimcSponge();
     });
 
+    it("Should calculate the mimc correctly", async () => {
+        const res = await mimcsponge["MiMCSponge"](1, 2, 3);
+        //console.log(res.xL.toString())
+        //console.log(res.xR.toString())
+        const res2 = mimc.hash(1, 2, 3);
+        //console.log(mimc.F.toString(res2.xL))
+        //console.log(mimc.F.toString(res2.xR))
+        assert.equal(res.xL.toString(), mimc.F.toString(res2.xL));
+        assert.equal(res.xR.toString(), mimc.F.toString(res2.xR));
+    })
+
+
     it('Should generate zeros correctly', async () => {
-        const zeros = generateZeros(10, mimc)
-        console.log(ethers.BigNumber.from(zeros[0]).toHexString())
-        console.log(ethers.BigNumber.from(zeros[1]).toHexString())
-        console.log(ethers.BigNumber.from(zeros[2]).toHexString())
-        console.log(ethers.BigNumber.from(zeros[3]).toHexString())
+        const zeros = generateZeros(mimc, 10)
+        //console.log(ethers.BigNumber.from(zeros[0]).toHexString())
+        //console.log(ethers.BigNumber.from(zeros[1]).toHexString())
+        //console.log(ethers.BigNumber.from(zeros[2]).toHexString())
+        //console.log(ethers.BigNumber.from(zeros[3]).toHexString())
 
         assert.equal(ethers.BigNumber.from(zeros[0]).toHexString(), '0x2fe54c60d3acabf3343a35b6eba15db4821b340f76e741e2249685ed4899af6c')
         assert.equal(ethers.BigNumber.from(zeros[1]).toHexString(), '0x256a6135777eee2fd26f54b8b7037a25439d5235caee224154186d2b8a52e31d')
@@ -41,36 +53,62 @@ describe("ZKTree Smart contract test", () => {
     })
 
     it("Should calculate hash correctly", async () => {
-        const zero = ethers.BigNumber.from('21663839004416932945382355908790599225266501822907911457504978515578255421292')    
+        const zero = ethers.BigNumber.from('21663839004416932945382355908790599225266501822907911457504978515578255421292')
 
         const res = await zktreetest.hashLeftRight(zero, zero);
-        console.log(res)
+        //console.log(res)
 
         const res2 = mimc.multiHash([zero, zero]);
-        console.log(ethers.BigNumber.from(mimc.F.toString(res2)).toHexString());
+        //console.log(ethers.BigNumber.from(mimc.F.toString(res2)).toHexString());
 
         assert.equal(res, ethers.BigNumber.from(mimc.F.toString(res2)).toHexString());
     })
+
+    it("Should calculate the root correctly", async () => {
+        const res = await zktreetest.getLastRoot();
+        // console.log(ethers.BigNumber.from(res).toHexString())
+
+        const res2 = calculateMerkleRoot(mimc, 10, [])
+        // console.log(ethers.BigNumber.from(res2).toHexString())
+
+        assert.equal(ethers.BigNumber.from(res).toHexString(), ethers.BigNumber.from(res2).toHexString());
+    })
+
 
     it("Should calculate the root correctly after deposit 1.", async () => {
         zktreetest.deposit(1);
 
         const res = await zktreetest.getLastRoot();
-        console.log(res)
+        // console.log(ethers.BigNumber.from(res).toHexString())
+
+        const res2 = calculateMerkleRoot(mimc, 10, [1])
+        // console.log(ethers.BigNumber.from(res2).toHexString())
+
+        assert.equal(ethers.BigNumber.from(res).toHexString(), ethers.BigNumber.from(res2).toHexString());
     })
 
     it("Should calculate the root correctly after deposit 2.", async () => {
         zktreetest.deposit(2);
 
         const res = await zktreetest.getLastRoot();
-        console.log(res)
+        // console.log(ethers.BigNumber.from(res).toHexString())
+
+        const res2 = calculateMerkleRoot(mimc, 10, [1, 2])
+        // console.log(ethers.BigNumber.from(res2).toHexString())
+
+        assert.equal(ethers.BigNumber.from(res).toHexString(), ethers.BigNumber.from(res2).toHexString());
     })
 
     it("Should calculate the root correctly after deposit 3.", async () => {
         zktreetest.deposit(3);
 
         const res = await zktreetest.getLastRoot();
-        console.log(res)
+        // console.log(ethers.BigNumber.from(res).toHexString())
+
+        const res2 = calculateMerkleRoot(mimc, 10, [1, 2, 3])
+        // console.log(ethers.BigNumber.from(res2).toHexString())
+
+        assert.equal(ethers.BigNumber.from(res).toHexString(), ethers.BigNumber.from(res2).toHexString());
     })
 
 })
