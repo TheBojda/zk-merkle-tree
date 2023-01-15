33c138db427382018f6e10db1438fa424f067887
Laszlo Fazekas <thebojda@gmail.com>
2023-01-15 10:07:02 +0100

    exporting r1cs into dist

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 mkdir -p build
-wget https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau -P ./build
+mkdir -p dist
+wget -nc https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau -P ./build
 circom test/circuits/CommitmentHasherTest.circom --wasm --r1cs -o ./build
 npx snarkjs groth16 setup build/CommitmentHasherTest.r1cs build/powersOfTau28_hez_final_15.ptau build/CommitmentHasherTest.zkey
 circom test/circuits/MerkleTreeCheckerTest.circom --wasm --r1cs -o ./build
 npx snarkjs groth16 setup build/MerkleTreeCheckerTest.r1cs build/powersOfTau28_hez_final_15.ptau build/MerkleTreeCheckerTest.zkey
-circom circuits/Verifier.circom --wasm --r1cs -o ./build
-npx snarkjs groth16 setup build/Verifier.r1cs build/powersOfTau28_hez_final_15.ptau build/Verifier.zkey
+circom circuits/Verifier.circom --r1cs -o ./dist
+circom circuits/Verifier.circom --wasm -o ./build
+npx snarkjs groth16 setup dist/Verifier.r1cs build/powersOfTau28_hez_final_15.ptau build/Verifier.zkey
 npx snarkjs zkey export verificationkey build/Verifier.zkey build/Verifier_vkey.json
 npx snarkjs zkey export solidityverifier build/Verifier.zkey contracts/Verifier.sol
 sed -i -e 's/pragma solidity \^0.6.11/pragma solidity 0.8.17/g' contracts/Verifier.sol
